9a8cdc8145f67d465df03f2d70945e804cc13f9f
fazurunisha1708 <108513744+fazurunisha1708@users.noreply.github.com>
2023-04-05 11:00:16 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Hooks from './Components/Hook';
+function App() {
+  return (
+    <div className="App">
+    <Hooks/>
+    </div>
+  );
+}
+
+export default App;
